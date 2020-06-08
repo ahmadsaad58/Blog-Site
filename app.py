3eed468951cf6412071a02e585114b6551e558e3
Saad Ahmad <ahmadsaad58@gmail.com>
2020-06-07 20:19:27 -0500

    playing around with flask

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask 
 
+
+
 app = Flask(__name__)
+
+
+@app.route('/<name>')
+def hello(name):
+	return 'Hello {}! How are you?'.format(name.title())
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
